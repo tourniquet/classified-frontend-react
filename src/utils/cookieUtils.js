31fb4a2268b47017437bbf6d992c6a5efab9d541
Ion Prodan <admyn3d@gmail.com>
2019-09-06 17:58:50 +0100

    Check if user is logged moved to a separate function

--- src/utils/cookieUtils.js
+++ src/utils/cookieUtils.js
@@ -3,3 +3,14 @@ export const getCookies = name => {
 
   return cookies.filter(el => el.split('=')[0] === name)
 }
+
+export const checkIfUserIsLogged = () => {
+  const email = getCookies('email').toString().replace('email=', '')
+  const id = getCookies('id').toString().replace('id=', '')
+
+  if (email && id) {
+    return { email, id }
+  }
+
+  return false
+}
